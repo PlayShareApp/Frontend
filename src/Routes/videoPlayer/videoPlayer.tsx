f8e32b974b051e3070cfc8c8ac484f6e471533da
Jonas <contact@jnsaph.com>
2022-06-03 21:24:30 +0200

    Preparing for Backend Communication

--- src/Routes/videoPlayer/videoPlayer.tsx
+++ src/Routes/videoPlayer/videoPlayer.tsx
@@ -1,5 +1,6 @@
 // Packages
 import React from 'react';
+import YouTube from "react-youtube";
 import { Button, Navbar, Container } from 'react-bootstrap';
 
 // CSS & Servcices
@@ -9,10 +10,48 @@ import './videoPlayer.css';
 import NavbarComponent from '../Components/Navbar/NavbarComponent';
 
 export default class VideoPlayerRoute extends React.Component {
+
+    /**
+     * @name opts
+     * @description Options for the YouTube player
+     */
+    private opts: Object = {
+        height: 480,
+        width: 853,
+        playerVars: {
+            controls: 1,
+            autoplay: 1
+        }
+    }
+
+    /**
+     * @name onReady
+     * @description Callback when the YouTube player is ready
+     * @param event 
+     */
+    private _onReady(event: any): void {
+        console.log(event)
+    }
+
+    /**
+     * @name onStateChange
+     * @description Callback when the YouTube player state changes
+     * @param event 
+     */
+    private onStateChange(event: any): void {
+        console.log(event.target.getCurrentTime())
+    }
+
+
     render() {
         return <div className="VideoPlayerRoute">
             <NavbarComponent/>
-            <h1>Imagine this being finished</h1>
+            <YouTube
+            className='videoPlayer'
+            opts={this.opts}
+            onReady={this._onReady}
+            onStateChange={this.onStateChange}
+            />
         </div>
     }
 }
